72c3348a52b53628374e07b381efd5d923a5947d
Just <grivten@yandex.ru>
2026-03-08 14:51:59 +0700

    init

---
+++ src/client/java/justs_js/twiw/TWIWModClient.java
@@ -0,0 +1,14 @@
+package justs_js.twiw;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TWIWModClient implements ClientModInitializer {
+	public static final String MOD_ID = "twiw";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
